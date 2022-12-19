1fac75199f2c10f9f5e33200b9ba97ed91d39c9e
keltoum <100529113+ouminaya@users.noreply.github.com>
2022-12-19 12:52:36 +0100

    création de l'entity recipe

--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -13,8 +13,53 @@ class Recipe
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $igredients = null;
+
+    #[ORM\Column(length: '0')]
+    private ?string $description = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getIgredients(): ?string
+    {
+        return $this->igredients;
+    }
+
+    public function setIgredients(string $igredients): self
+    {
+        $this->igredients = $igredients;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
     }
 }
