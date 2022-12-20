05468a9be2001915e0812fb3df637fc27b57ef1c
keltoum <100529113+ouminaya@users.noreply.github.com>
2022-12-20 17:14:39 +0100

    creatin fichier + appel des données avec findAll

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\IngredientsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IngredientController extends AbstractController
+{
+    #[Route('/ingredient', name: 'app_ingredient')]
+    public function index(IngredientsRepository $repository): Response
+    {
+        $ingredients = $repository->findAll();
+        
+        return $this->render('pages/ingredient/index.html.twig', ['ingredients' => $ingredients]);
+    }
+}
