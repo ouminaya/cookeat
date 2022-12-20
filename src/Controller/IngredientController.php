b3776de93b9db22e083b9a96e1ed97a5caf939b5
keltoum <100529113+ouminaya@users.noreply.github.com>
2022-12-20 19:18:32 +0100

    mise en place de la pagination+changement du style

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use App\Repository\IngredientsRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IngredientController extends AbstractController
 {
     #[Route('/ingredient', name: 'app_ingredient')]
-    public function index(IngredientsRepository $repository): Response
+    public function index(IngredientsRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
-        
-        return $this->render('pages/ingredient/index.html.twig', ['ingredients' => $repository->findAll()]);
+        $ingredients = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+        return $this->render('pages/ingredient/index.html.twig', ['ingredients' => $ingredients]);
     }
 }
