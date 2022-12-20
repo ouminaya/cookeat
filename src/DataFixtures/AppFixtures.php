515a729d8db58755278393f6d269355364674bfd
keltoum <100529113+ouminaya@users.noreply.github.com>
2022-12-20 16:29:30 +0100

    import class generator

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,12 +2,25 @@
 
 namespace App\DataFixtures;
 
+use Faker\Generator;
 use App\Entity\Ingredients;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Console\CommandLoader\FactoryCommandLoader;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var Generator
+     */
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+    
+
     public function load(ObjectManager $manager): void
     {
         for ($i = 0; $i < 50; $i++) {
@@ -17,10 +30,6 @@ class AppFixtures extends Fixture
 
                 $manager->persist($Ingredients);
         }
-
-
-       
-
 
 
         $manager->flush();
