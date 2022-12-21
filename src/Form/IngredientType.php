3cd2cfb29f3cf4e7b0331485b4277dea1012c8a3
keltoum <100529113+ouminaya@users.noreply.github.com>
2022-12-21 19:32:37 +0100

    contraints form

--- src/Form/IngredientType.php
+++ src/Form/IngredientType.php
@@ -4,17 +4,49 @@ namespace App\Form;
 
 use App\Entity\Ingredients;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class IngredientType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('quantity')
-        ;
+            ->add('name', TextType::class,[
+                'attr' => [
+                    'class'=> 'form-control',
+                    'minlength' => '2', 
+                    'maxlength' => '50'             
+                ],
+                  'label' => 'Nom',
+                  'label_attr' => [
+                    new Assert\Length(['min' => 2, 'max' => 50 ]),
+                    new Assert\NotBlank()
+                  ]
+            ])
+            
+
+            ->add('quantity' , NumberType::class,[
+                'attr' => [
+                'class'=> 'form-control',
+                         
+            ],
+              'label' => 'Quantity',
+              'label_attr' => [
+                new Assert\Positive(),
+                // new Assert\lessThan(1000)
+              ]
+                
+            ])
+            ->add('submit', SubmitType::class,[
+                'attr' => ['btn btn-success mt-4'
+            ],
+            'label'=> 'Créer mon ingrédient' 
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
