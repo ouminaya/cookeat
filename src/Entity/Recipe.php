7578bc23c88b1ac9dafc92f5c2c15ca0290aa028
keltoum <100529113+ouminaya@users.noreply.github.com>
2022-12-22 12:12:28 +0100

    creation + modif de l'entity recipe+ vérification des ASSERT

--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -3,8 +3,17 @@
 namespace App\Entity;
 
 use App\Repository\RecipeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+use function PHPSTORM_META\type;
+
+#[UniqueEntity('name')]
+#[ORM\HasLifecycleCallbacks]
 #[ORM\Entity(repositoryClass: RecipeRepository::class)]
 class Recipe
 {
@@ -13,11 +22,58 @@ class Recipe
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 50)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 2, max: 50)]
     private ?string $name = null;
 
-    #[ORM\Column(length: '0')]
+    #[ORM\Column(length: 'text')]
+    #[Assert\NotBlank()]
     private ?string $description = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Assert\Positive()]
+    #[Assert\LessThan(1441)]
+    private ?int $time = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $nbPeople = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Assert\Positive()]
+    #[Assert\LessThan(6)]
+    private ?int $difficulty = null;
+
+    #[ORM\Column]
+    private ?bool $isFavorite = null;
+
+    #[ORM\Column]
+    #[Assert\NotNull(type:'datetime_immutable')]
+    private ?\DateTimeImmutable $createAt = null;
+
+    #[ORM\Column]
+    #[Assert\NotNull(type:'datetime_immutable')]
+    private ?\DateTimeImmutable $updateAt = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $ingredients = null;
+
+    #[ORM\ManyToMany(targetEntity: ingredients::class)]
+    private Collection $relation;
+
+    public function __construct()
+    {
+        $this->relation = new ArrayCollection();
+        $this->createAt = new \DateTimeImmutable;
+        $this->updateAt = new \DateTimeImmutable;
+    }
+
+    #[ORM\PrePersist()]
+    public function setUpdateAtValue()
+    {
+        $this->updateAt = new \DateTimeImmutable;
+
+    }
 
     public function getId(): ?int
     {
@@ -44,6 +100,102 @@ class Recipe
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTime(): ?int
+    {
+        return $this->time;
+    }
+
+    public function setTime(?int $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getNbPeople(): ?int
+    {
+        return $this->nbPeople;
+    }
+
+    public function setNbPeople(?int $nbPeople): self
+    {
+        $this->nbPeople = $nbPeople;
+
+        return $this;
+    }
+
+    public function getDifficulty(): ?int
+    {
+        return $this->difficulty;
+    }
+
+    public function setDifficulty(?int $difficulty): self
+    {
+        $this->difficulty = $difficulty;
+
+        return $this;
+    }
+
+    public function isIsFavorite(): ?bool
+    {
+        return $this->isFavorite;
+    }
+
+    public function setIsFavorite(bool $isFavorite): self
+    {
+        $this->isFavorite = $isFavorite;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeImmutable
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(\DateTimeImmutable $updateAt): self
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+
+    public function getIngredients(): ?string
+    {
+        return $this->ingredients;
+    }
+
+    public function setIngredients(string $ingredients): self
+    {
+        $this->ingredients = $ingredients;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ingredients>
+     */
+    public function getRelation(): Collection
+    {
+        return $this->relation;
+    }
+
+    public function addRelation(ingredients $relation): self
+    {
+        if (!$this->relation->contains($relation)) {
+            $this->relation->add($relation);
+        }
+
+        return $this;
+    }
+
+    public function removeRelation(ingredients $relation): self
+    {
+        $this->relation->removeElement($relation);
 
         return $this;
     }
