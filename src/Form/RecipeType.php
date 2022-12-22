418987f0909afeff55b469a5287ebf383419d867
keltoum <100529113+ouminaya@users.noreply.github.com>
2022-12-22 20:40:13 +0100

    param+constraints

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -99,14 +99,14 @@ class RecipeType extends AbstractType
             ])
             ->add('isFavorite', CheckboxType::class, [
             'attr' => [
-                'class' => 'form-control',
-                'required' => ''
+                'class' => 'form-check-input',
+                
 
             ],
             'required' => 'false',
             'label' => 'Favoris?',
             'label_attr' => [
-                'class' => 'form-label mt-4'
+                'class' => 'form-check-label'
             ],
             'constraints' => [
                 new Assert\NotNull()
@@ -132,7 +132,7 @@ class RecipeType extends AbstractType
                 'attr' => [
                     'btn btn-success mt-4'
                 ],
-                'label' => 'Créer ma recette'
+                'label' => 'créer ma recette'
             ]);;
     }
 
