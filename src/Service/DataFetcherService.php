4d5ff6d0bc54771e2fbcb26433203aae4249a39a
keltoum <100529113+ouminaya@users.noreply.github.com>
2022-12-26 20:59:35 +0100

    recupération des recettes dpuis une api

---
+++ src/Service/DataFetcherService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class DataFetcherService
+{
+    public function __construct(
+        private HttpClientInterface $client,
+    ) {
+        $this->client = $client;
+    }
+
+    public function fetchData(): array
+    {
+        $content = [];
+
+        for ($i = 0; $i < 2; $i++) { 
+            $response = $this->client->request(
+                'GET',
+                'https://api.spoonacular.com/recipes/random',
+                [
+                    'query' => [
+                        'apiKey' => 'c560e416d8fb4269b0235ddc8a262fbd',
+                        'number' => 1
+                    ],
+                ]
+            );
+
+            $content[] = $response->toArray();
+        }
+
+        return $content;
+    }
+}
